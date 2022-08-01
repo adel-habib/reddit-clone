4d99e57d01d8045262f3fcc65c06cc4f185c5b2a
ahmed <65723807+adel-habib@users.noreply.github.com>
2022-08-02 01:10:15 +0200

    Register form / graphql fetch

--- lireddit-web/src/pages/_app.tsx
+++ lireddit-web/src/pages/_app.tsx
@@ -2,12 +2,16 @@ import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from '../theme'
 import { AppProps } from 'next/app'
+import { createClient, Provider } from 'urql'
 
+const client = createClient({url: 'http://localhost:4000/graphql'})
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <Provider value={client }>
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
+    </Provider>
   )
 }
 
--- lireddit-web/src/pages/register.tsx
+++ lireddit-web/src/pages/register.tsx
@@ -1,30 +1,47 @@
 import React from 'react'
-import {Formik, Form} from "formik"
-import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/react'
+import { Formik, Form } from "formik"
+import { FormControl, FormLabel, Input, FormErrorMessage, Box, Button } from '@chakra-ui/react'
 import { Wrapper } from '../components/Wrapper'
+import { InputField } from '../components/InputField'
+import { useMutation } from 'urql'
 interface registerProps {
 
 }
 
+const REGISTER_MUT = `mutation register($username : String!, $password: String!) {
+  Register(options: {username: $username, password: $password})
+  {
+    user{
+      id
+    	username
+    }
+    errors{
+      field
+      message
+    }
+  }
+}
+`
 
-  
 
-export const Register: React.FC<registerProps> = ({}) => {
-        return(
-        <Wrapper variant='small'> 
-        <Formik  initialValues={{ username: '',password: '' }} onSubmit={ (values) => {console.log("values")}}>
-            {({values,handleChange}) => (
-                <Form>
-                <FormControl >
-                <FormLabel htmlFor='username'>Username</FormLabel>
-                <Input value={values.username} onChange={handleChange} id='username' placeholder='username' />
-                {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
-              </FormControl>
-                </Form>
-            )}
-        </Formik>
-        </Wrapper>
-        )
+export const Register: React.FC<registerProps> = ({ }) => {
+  const [, register] = useMutation(REGISTER_MUT)
+  return (
+    <Wrapper variant='small'>
+      <Formik initialValues={{ username: '', password: '' }} onSubmit={(values) => { register(values) }}>
+        {({ isSubmitting }) => (
+          <Box mt={4}>
+            <Form>
+              <InputField name='username' placeholder='username' label='username'></InputField>
+              <InputField name='password' placeholder='Password' label='Password' type='password'></InputField>
+              <Button mt={4} type='submit' isLoading={isSubmitting}> Register</Button>
+            </Form>
+          </Box>
+
+        )}
+      </Formik>
+    </Wrapper>
+  )
 }
 
 export default Register
